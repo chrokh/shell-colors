fb5679b14192b8dc9d276390a4ed79147f6f7d80
chrokh <christopher.okhravi@gmail.com>
2016-08-06 00:06:21 +0200

    Base-16 dependency has changed. 1) Dark/light are not always specified in scheme name. 2) Path to colorscheme shell files have changed.

--- shell-colors.sh
+++ shell-colors.sh
@@ -7,23 +7,20 @@
 export SHELL_COLOR_CONF="$HOME/.bashbg"
 export VIM_COLOR_CONF="$HOME/.vimbg"
 function theme {
-  local default_name="default"
-  local default_back="dark"
+  local default_name="default-dark"
 
   local name="${1:-$(echo $default_name)}"
-  local back="${2:-$(echo "dark")}"
-  local base="$HOME/.config/base16-shell/"
+  local base="$HOME/.config/base16-shell/scripts/"
   local prfx="base16-"
   local sufx=".sh"
 
   local them=${prfx}${name}
-  local file=${base}${them}.${back}${sufx}
+  local file=${base}${them}${sufx}
 
   # Verbose
   echo "vim"
   echo "  |__ conf        $VIM_COLOR_CONF"
   echo "  |__ theme       $them"
-  echo "  |__ background  $back"
   echo "shell"
   echo "  |__ conf        $SHELL_COLOR_CONF"
   echo "  |__ theme       $file"
@@ -33,7 +30,6 @@ function theme {
   source $SHELL_COLOR_CONF
 
   # Set vim colors
-  echo "set background=$back" > $VIM_COLOR_CONF
   echo "colorscheme $them" >> $VIM_COLOR_CONF
 }
 
